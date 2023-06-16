569da1656c8072851ab8b34695797786c63622d4
Valeria48562 <vblagodatskaja@gmail.com>
2023-06-16 17:58:37 +0300

    main

--- script.js
+++ script.js
@@ -39,8 +39,6 @@ let displayArea = document.getElementById("result-display");
 let prevValue = "";
 
 function updateDisplay(buttonValue) {
-  prevValue = displayArea.innerHTML;
-
   if (hasPressedOperator) {
     displayArea.innerHTML = "";
     hasPressedOperator = false;
@@ -52,15 +50,19 @@ function updateDisplay(buttonValue) {
 function getOperator(operator) {
   currentOperator = operator;
   hasPressedOperator = true;
+  prevValue += displayArea.innerHTML;
   console.log(currentOperator);
 }
 calcButton.addEventListener("click", () => {
-  console.log("prevValue", prevValue);
-  console.log("current value", displayArea.innerHTML);
-  console.log("currentOperator", currentOperator);
   displayArea.innerHTML = calculate(
     parseFloat(prevValue),
     currentOperator,
     parseFloat(displayArea.innerHTML)
   );
 });
+function deleteOneValue() {
+  displayArea.innerHTML = displayArea.innerHTML.slice(0, -1);
+}
+function deleteAll() {
+  displayArea.innerHTML = "";
+}
