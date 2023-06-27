bd3422262eeb426604e1cb61c65c5652b161302f
Valeria48562 <vblagodatskaja@gmail.com>
2023-06-27 15:10:29 +0300

    main

--- script.js
+++ script.js
@@ -5,28 +5,23 @@ function updateDisplay(buttonValue) {
   const currentFormula = displayArea.innerHTML;
   const lastCharacter = currentFormula[currentFormula.length - 1];
 
-  // Check if the last character is an operator
   if (
     lastCharacter === "+" ||
     lastCharacter === "-" ||
     lastCharacter === "*" ||
     lastCharacter === "/"
   ) {
-    // Check if the current button value is also an operator
     if (
       buttonValue === "+" ||
       buttonValue === "-" ||
       buttonValue === "*" ||
       buttonValue === "/"
     ) {
-      // Replace the last operator with the new one
       displayArea.innerHTML = currentFormula.slice(0, -1) + buttonValue;
     } else {
-      // Append the button value to the display
       displayArea.innerHTML += buttonValue;
     }
   } else {
-    // Append the button value to the display
     displayArea.innerHTML += buttonValue;
   }
 }
